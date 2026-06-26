68a16dc8c997b67305a6c38e5209c748aae8cfaf
webutilityhub-pro <1921008yes@gmail.com>
2026-06-26 18:58:39 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,3 +1,8 @@
-// ads.js - Test code
-alert('Script is running!');
-window.open('https://www.google.com', '_blank');
+// ads.js
+document.addEventListener('click', function() {
+    if (!localStorage.getItem('ad_shown')) {
+        window.open('https://www.effectivecpmnetwork.com/cdtzbhi2rm?key=ab97b4527f155e794eabafc6f63ce7e8', '_blank');
+        localStorage.setItem('ad_shown', 'true');
+    }
+}, { once: true });
+
